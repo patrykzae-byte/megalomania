9b1b418dce5c983bf5c6cabdfafecca272568636
Patryk <patryk@Patryks-MacBook-Air.local>
2026-03-17 19:37:40 +0100

    dd

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ function App() {
     <div>
       KONIEC PRACY
       <div style={{ marginTop: '20px' }}>
+
+
+
         <button onClick={() => setCount(count - 1)}>Odejmij</button>
+
+
+
         <span style={{ margin: '0 10px' }}>Licznik: {count}</span>
+
+
+        <button onClick={() => setCount(count * 1)}>pomnoz</button>
+
         <button onClick={() => setCount(count + 1)}>Dodaj</button>
       </div>
     </div>
